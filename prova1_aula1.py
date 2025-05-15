b94f0de46ce9f0ee706dcebee7fdec572df12475
pieronig <guipieronii@gmail.com>
2025-05-14 23:06:15 -0300

    Lógica da Programação - prova1_aula1

---
+++ prova1_aula1.py
@@ -0,0 +1,16 @@
+print("Bem-vindo!")
+
+nome = input("Digite seu nome: ")
+
+print(f"Olá, {nome}, hoje calcularemos a área de um retângulo.")
+
+print("Para definirmos o valor, utilizaremos a formula: Àrea=Base×Altura.")
+
+base = float(input("Primeiro, informe o tamanho da BASE do retângulo: "))
+
+altura = float(input("Agora, me informe a ALTURA do retângulo: "))
+
+print("Então de acordo com suas informações, a área do retângulo é de:")
+
+print(f"{base*altura}m²")
+
